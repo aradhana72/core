1a796c5875279081164f1db36e5f27d2f982d7e4
Steven A White <sawhite@ara.com>
2021-02-17 14:48:07 -0500

    ASDAS3

--- projects/biogears/libBiogears/src/io/cdm/Circuit.h
+++ projects/biogears/libBiogears/src/io/cdm/Circuit.h
@@ -152,7 +152,7 @@ namespace io {
   void Circuit::UnMarshall(const SECircuit<CIRCUIT_TYPES>& in, const CircuitBindType& out)
   {
     out.Name(in.m_Name);
-    if (HasReferenceNode()) {
+    if (in.HasReferenceNode()) {
       for (NodeType* n : in.m_ReferenceNodes)
         out.ReferenceNode().push_back(n->GetName());
     }
